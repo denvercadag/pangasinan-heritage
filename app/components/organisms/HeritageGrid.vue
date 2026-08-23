ea04f84c152a91a7eff717ba280846dc0329a661
denvercadag <denvercadag2005@gmail.com>
2026-08-24 02:41:44 +0800

    Fix heritage image paths

--- app/components/organisms/HeritageGrid.vue
+++ app/components/organisms/HeritageGrid.vue
@@ -1,21 +1,21 @@
 <template>
   <section class="heritage-grid">
     <HeritageCard
-      image="/images/hundred-islands.jpg"
+      image="images/hundred-islands.jpg"
       title="Hundred Islands"
       location="Alaminos, Pangasinan"
       description="Explore the famous islands and clear blue waters of one of Pangasinan's most popular destinations."
     />
 
     <HeritageCard
-      image="/images/bolinao-lighthouse.jpg"
+      image="images/bolinao-lighthouse.jpg"
       title="Bolinao Lighthouse"
       location="Bolinao, Pangasinan"
       description="Discover this historic lighthouse overlooking the beautiful coast of Bolinao."
     />
 
     <HeritageCard
-      image="/images/balungao-hot-spring.jpg"
+      image="images/balungao-hot-spring.jpg"
       title="Balungao Hot Spring"
       location="Balungao, Pangasinan"
       description="Relax and enjoy the natural hot spring located at the foot of Mount Balungao."
